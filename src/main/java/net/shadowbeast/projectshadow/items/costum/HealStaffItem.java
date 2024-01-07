4c802d1a160ba7e482c3f6ed212a97c996f98aa2
TheshadowLP <144700529+TheshadowLP@users.noreply.github.com>
2024-01-07 17:42:22 +0100

    Changed Regeneration to Instant Health from the Staff

--- src/main/java/net/shadowbeast/projectshadow/items/costum/HealStaffItem.java
+++ src/main/java/net/shadowbeast/projectshadow/items/costum/HealStaffItem.java
@@ -12,25 +12,21 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class HealStaffItem extends Item {
     public HealStaffItem(Properties pProperties) {
         super(pProperties);
     }
-
     private int timer;
-    private int timerMax = 1200; //The time you want for the timer in ticks
-
-
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, @NotNull Player pPlayer, @NotNull InteractionHand pUsedHand) {
 
         if (this.timer <= 0) //check if timer is less or equal than 0
         {
             if (!pLevel.isClientSide()) //check if the item is used on the server and not the client
             {
-                timer = timerMax;
+                timer = 1200; //The time you want for the timer in ticks
                 pPlayer.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 20, false, false)); //Apply the effect to the player
-
 
                 pPlayer.getItemInHand(pUsedHand).hurtAndBreak(1, pPlayer,
                         player1 -> pPlayer.broadcastBreakEvent(pPlayer.getUsedItemHand()));
@@ -39,22 +35,16 @@ public class HealStaffItem extends Item {
             pPlayer.playSound(SoundEvents.ALLAY_THROW, 1f, 1f);
             pPlayer.playSound(SoundEvents.AMETHYST_BLOCK_CHIME, 1f, 1f);
 
-
         }
         else //if timer is more than 0
         {
-
             pPlayer.playSound(SoundEvents.CHEST_LOCKED, 0.1f, 2f);
         }
 
         return ItemUtils.startUsingInstantly(pLevel, pPlayer, pUsedHand);
     }
-
-
-
-
     @Override
-    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+    public void inventoryTick(@NotNull ItemStack pStack, @NotNull Level pLevel, @NotNull Entity pEntity, int pSlotId, boolean pIsSelected) {
         if(timer >= 0){
             timer--;
         }
