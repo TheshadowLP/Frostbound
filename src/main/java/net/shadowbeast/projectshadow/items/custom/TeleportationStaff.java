a6f018234f5d209d645bcfb08eab4e265a657927
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-15 13:22:10 -0500

    Created class

---
+++ src/main/java/net/shadowbeast/projectshadow/items/custom/TeleportationStaff.java
@@ -0,0 +1,9 @@
+package net.shadowbeast.projectshadow.items.custom;
+
+import net.minecraft.world.item.Item;
+
+public class TeleportationStaff extends Item {
+    public TeleportationStaff(Properties pProperties) {
+        super(pProperties);
+    }
+}
