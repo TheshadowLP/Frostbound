df5ae1ac8afaf44ccb7951672c18031249255df4
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-19 12:07:02 -0500

    Added "@NotNull"

--- src/main/java/net/shadowbeast/projectshadow/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/shadowbeast/projectshadow/worldgen/ModConfiguredFeatures.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvi
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.shadowbeast.projectshadow.ProjectShadow;
 import net.shadowbeast.projectshadow.blocks.ModBlocks;
+import org.jetbrains.annotations.NotNull;
 
 public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> FROZEN_KEY = registerKey("frozen");
@@ -29,9 +30,9 @@ public class ModConfiguredFeatures {
                 new BlobFoliagePlacer(ConstantInt.of(3), ConstantInt.of(2), 3),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
     }
-    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+    public static @NotNull ResourceKey<ConfiguredFeature<?, ?>> registerKey(@NotNull String name) {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(ProjectShadow.MOD_ID, name));
     }
-    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {context.register(key, new ConfiguredFeature<>(feature, configuration));
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(@NotNull BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {context.register(key, new ConfiguredFeature<>(feature, configuration));
     }
 }
