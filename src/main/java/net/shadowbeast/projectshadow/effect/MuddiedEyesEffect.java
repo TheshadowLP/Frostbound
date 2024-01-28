e8e50b01c394fecf35c64040b8ba1f21d231d305
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-28 12:34:29 -0500

    Added slowness and blindness

---
+++ src/main/java/net/shadowbeast/projectshadow/effect/MuddiedEyesEffect.java
@@ -0,0 +1,29 @@
+package net.shadowbeast.projectshadow.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
+
+public class MuddiedEyesEffect extends MobEffect {
+    public MuddiedEyesEffect(MobEffectCategory mobEffectCategory, int color) {
+        super(mobEffectCategory, color);
+    }
+
+
+
+    @Override
+    public void applyEffectTick(@NotNull LivingEntity pLivingEntity, int pAmplifier) {
+        if (!pLivingEntity.level().isClientSide()) {
+            pLivingEntity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 120, 1));
+            pLivingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 120, 2));
+        }
+        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {return true;}
+}
