c8f0df4383b898b9890b69618a0ccabae00b446d
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-28 12:47:30 -0500

    Removed unused import

--- src/main/java/net/shadowbeast/projectshadow/effect/MuddiedEyesEffect.java
+++ src/main/java/net/shadowbeast/projectshadow/effect/MuddiedEyesEffect.java
@@ -4,7 +4,6 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
 
