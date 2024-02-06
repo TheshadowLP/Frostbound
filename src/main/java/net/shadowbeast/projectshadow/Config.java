80e0157bcd4f6a8b97fccff011172a989c4385a7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-02-06 16:51:44 -0500

    Removed unused import statements

--- src/main/java/net/shadowbeast/projectshadow/Config.java
+++ src/main/java/net/shadowbeast/projectshadow/Config.java
@@ -1,20 +1,12 @@
 package net.shadowbeast.projectshadow;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
+
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ProjectShadow.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
