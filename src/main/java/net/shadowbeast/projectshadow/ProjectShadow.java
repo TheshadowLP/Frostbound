c74e29275bbbe75b7d9dcb3093578d9b08520ce3
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-02-06 09:32:12 -0500

    Registered CONFIG files

--- src/main/java/net/shadowbeast/projectshadow/ProjectShadow.java
+++ src/main/java/net/shadowbeast/projectshadow/ProjectShadow.java
@@ -10,7 +10,9 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -45,6 +47,7 @@ public class ProjectShadow {
         ModRecipes.register(modEventBus);
         ModMenuTypes.register(modEventBus);
         modEventBus.addListener(this::clientSetup);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
