e11094fd6fb74bb7eb2777191718ed6cfc82115a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-02-07 09:34:37 -0500

    Need a way to tell FORGE to use this

--- src/main/java/net/shadowbeast/projectshadow/Config.java
+++ src/main/java/net/shadowbeast/projectshadow/Config.java
@@ -1,23 +1,20 @@
 package net.shadowbeast.projectshadow;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Properties;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
+
+
 @Mod.EventBusSubscriber(modid = ProjectShadow.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    private static final Properties defaultValues = new Properties();
+    private String fileName;
 
     private static final ForgeConfigSpec.BooleanValue SNOWBALLS_DOES_DAMAGE = BUILDER
             .comment("Whether snowballs do damage")
@@ -32,6 +29,5 @@ public class Config {
     static void onLoad(final ModConfigEvent event)
     {
         snowballsDoesDamage = SNOWBALLS_DOES_DAMAGE.get();
-
     }
 }
--- src/main/java/net/shadowbeast/projectshadow/ConfigScreen.java
+++ src/main/java/net/shadowbeast/projectshadow/ConfigScreen.java
@@ -1,12 +1,37 @@
 package net.shadowbeast.projectshadow;
 
 
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
 
 public class ConfigScreen extends Screen {
+    private final Screen previous;
 
-    protected ConfigScreen(Component pTitle) {
-        super(pTitle);
+    public ConfigScreen(Screen previous) {
+        super(Component.literal("Mouse Tweaks Options"));
+        this.previous = previous;
+    }
+
+    @Override
+    protected void init() {
+        this.addRenderableWidget(CycleButton.onOffBuilder(Config.snowballsDoesDamage)
+                .create(this.width / 2 - 155, this.height / 6, 150, 20,
+                        Component.literal("Do Snowballs Do Damage"), (button, value) -> Config.snowballsDoesDamage = value));
+    }
+
+    @Override
+    public void onClose() {
+        assert this.minecraft != null;
+        this.minecraft.setScreen(previous);
+    }
+
+    @Override
+    public void render(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        this.renderDirtBackground(pGuiGraphics);
+        pGuiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 15, 0xFFFFFF);
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
     }
 }
