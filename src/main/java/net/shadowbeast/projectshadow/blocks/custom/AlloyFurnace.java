3db4903f7c94805ab5b6861ba569e04a6fdfd9cc
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-02-20 12:20:08 -0500

    Block faces player direction when placed

--- src/main/java/net/shadowbeast/projectshadow/blocks/custom/AlloyFurnace.java
+++ src/main/java/net/shadowbeast/projectshadow/blocks/custom/AlloyFurnace.java
@@ -1,14 +1,21 @@
 package net.shadowbeast.projectshadow.blocks.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+
 
 @SuppressWarnings("deprecated")
 @ParametersAreNonnullByDefault
@@ -17,12 +24,28 @@ public class AlloyFurnace extends BaseEntityBlock {
         super(pProperties);
     }
 
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+
+
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return null;
     }
     //TODO - the gui
+
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
 
     @Override
     public @NotNull RenderShape getRenderShape(BlockState pState) {
