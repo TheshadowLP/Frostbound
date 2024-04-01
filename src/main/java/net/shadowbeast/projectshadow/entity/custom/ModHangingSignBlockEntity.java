0864ab47ae6aaf1ef257f2e2edfb7fa0d0b598ce
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-01 12:43:28 -0400

    Changed the class to extend HangingSignBlockEntity and not SignBlockEntity

--- src/main/java/net/shadowbeast/projectshadow/entity/custom/ModHangingSignBlockEntity.java
+++ src/main/java/net/shadowbeast/projectshadow/entity/custom/ModHangingSignBlockEntity.java
@@ -2,15 +2,17 @@ package net.shadowbeast.projectshadow.entity.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.shadowbeast.projectshadow.entity.ModBlockEntities;
 import org.jetbrains.annotations.NotNull;
 
-public class ModHangingSignBlockEntity extends SignBlockEntity {
+public class ModHangingSignBlockEntity extends HangingSignBlockEntity {
     public ModHangingSignBlockEntity(BlockPos blockPos, BlockState blockState) {
-        super(ModBlockEntities.MOD_HANGING_SIGN.get(), blockPos, blockState);
+        super(blockPos, blockState);
     }
+
     @Override
     public @NotNull BlockEntityType<?> getType() {
         return ModBlockEntities.MOD_HANGING_SIGN.get();
