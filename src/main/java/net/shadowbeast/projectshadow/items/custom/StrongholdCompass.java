f6e523e01427cdcaaeeea6d3ae85d8e699e0fc6a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-03 12:40:41 -0400

    Added compass

---
+++ src/main/java/net/shadowbeast/projectshadow/items/custom/StrongholdCompass.java
@@ -0,0 +1,22 @@
+package net.shadowbeast.projectshadow.items.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class StrongholdCompass extends Item {
+    public StrongholdCompass(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("item.projectshadow.stronghold_compass.description"));
+    }
+}
