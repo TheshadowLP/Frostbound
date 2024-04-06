5415eef66729e7f1354c19be095bc21abc0232c3
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-06 08:07:14 -0400

    Added missing @OnlyIn(Dist.CLIENT)

--- src/main/java/net/shadowbeast/projectshadow/items/custom/ModItemProperties.java
+++ src/main/java/net/shadowbeast/projectshadow/items/custom/ModItemProperties.java
@@ -2,8 +2,11 @@ package net.shadowbeast.projectshadow.items.custom;
 
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.shadowbeast.projectshadow.items.ModItems;
 
+@OnlyIn(Dist.CLIENT)
 public class ModItemProperties {
     public static void addCustomItemProperties() {
         ItemProperties.register(ModItems.AQUANIUM_SHIELD.get(), new ResourceLocation("blocking"), (p_174575_, p_174576_, p_174577_, p_174578_) -> p_174577_ != null && p_174577_.isUsingItem() && p_174577_.getUseItem() == p_174575_ ? 1.0F : 0.0F);
