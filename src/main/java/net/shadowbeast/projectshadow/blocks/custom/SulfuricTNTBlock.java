14efe21da4901acb819ce0de4dd1ffd0d76c9509
Siepert123 <sj.hijmans@gmail.com>
2024-04-08 16:05:21 +0200

    sulfuric tnt doesn't really have a fuse, but it can explode

--- src/main/java/net/shadowbeast/projectshadow/blocks/custom/SulfuricTNTBlock.java
+++ src/main/java/net/shadowbeast/projectshadow/blocks/custom/SulfuricTNTBlock.java
@@ -1,18 +1,25 @@
 package net.shadowbeast.projectshadow.blocks.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+
+import static net.minecraft.world.level.block.RedStoneWireBlock.POWER;
 
 @ParametersAreNonnullByDefault
 @SuppressWarnings("deprecation")
@@ -25,6 +32,7 @@ public class SulfuricTNTBlock extends Block {
     public @NotNull InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         ItemStack handItem = pPlayer.getItemInHand(pHand);
         if (handItem.is(Items.FLINT_AND_STEEL)) {
+            pLevel.setBlock(pPos, Blocks.AIR.defaultBlockState(), 3);
             pLevel.explode(pPlayer, pPos.getX() + 0.5, pPos.getY() + 0.5, pPos.getZ() + 0.5, 7.5f, true, Level.ExplosionInteraction.TNT);
             return InteractionResult.SUCCESS;
         } else {
@@ -32,4 +40,10 @@ public class SulfuricTNTBlock extends Block {
         }
     }
 
+    @Override
+    public void onBlockExploded(BlockState state, Level level, BlockPos pos, Explosion explosion) {
+        level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
+        level.explode(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 7.5f, true, Level.ExplosionInteraction.TNT);
+        super.onBlockExploded(state, level, pos, explosion);
+    }
 }
