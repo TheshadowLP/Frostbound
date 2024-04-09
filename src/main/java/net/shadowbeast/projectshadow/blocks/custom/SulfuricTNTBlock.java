345a393d6bd84b9502fdce219ec94e8156b005b5
Shadowkiller548 <92207062+Shadowkiller548@users.noreply.github.com>
2024-04-09 18:15:52 +0200

    tags added

--- src/main/java/net/shadowbeast/projectshadow/blocks/custom/SulfuricTNTBlock.java
+++ src/main/java/net/shadowbeast/projectshadow/blocks/custom/SulfuricTNTBlock.java
@@ -1,16 +1,13 @@
 package net.shadowbeast.projectshadow.blocks.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,8 +15,6 @@ import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-import static net.minecraft.world.level.block.RedStoneWireBlock.POWER;
 
 @ParametersAreNonnullByDefault
 @SuppressWarnings("deprecation")
