a4ad013dea07643c785895b006b7586527539592
luhcarti <tunefreddie@gmail.com>
2024-04-23 12:51:12 +0200

    Added new data gens

---
+++ src/main/java/net/shadowbeast/projectshadow/datagen/ModBlockStateProvider.java
@@ -0,0 +1,26 @@
+package net.shadowbeast.projectshadow.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.*;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.shadowbeast.projectshadow.ProjectShadow;
+import net.shadowbeast.projectshadow.blocks.ModBlocks;
+
+import java.util.function.Function;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, ProjectShadow.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
