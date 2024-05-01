8dc30a78a245fac06fab030b0cd4ec8c282070f3
Tejty <damian.bonus@gmail.com>
2024-05-01 10:28:59 +0200

    Crops finished
    Added 7th texture for pepper crop, added cross model for both, and fixed hitbox for both

--- src/main/java/net/shadowbeast/projectshadow/blocks/crops/KohlrabiCropBlock.java
+++ src/main/java/net/shadowbeast/projectshadow/blocks/crops/KohlrabiCropBlock.java
@@ -1,5 +1,7 @@
 package net.shadowbeast.projectshadow.blocks.crops;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
@@ -7,15 +9,22 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.shadowbeast.projectshadow.items.ModItems;
 import org.jetbrains.annotations.NotNull;
 
 //Credits for Kohlrabi textures to Kaupenjoe, in future custom texture will be added
 public class KohlrabiCropBlock extends CropBlock {
     public static final int MAX_AGE = 6;
+    private static final VoxelShape[] SHAPE_BY_AGE;
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 6);
     public KohlrabiCropBlock(BlockBehaviour.Properties pProperties) {
         super(pProperties);
+    }
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE_BY_AGE[this.getAge(pState)];
     }
     @Override
     protected @NotNull ItemLike getBaseSeedId() {
@@ -32,5 +41,16 @@ public class KohlrabiCropBlock extends CropBlock {
     @Override
     protected void createBlockStateDefinition(@NotNull StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(AGE);
+    }
+    static{
+        SHAPE_BY_AGE = new VoxelShape[]{
+                Block.box(5.0, 0.0, 5.0, 11.0, 2.0, 11.0),
+                Block.box(4.0, 0.0, 4.0, 12.0, 4.0, 12.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 7.0, 13.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 9.0, 13.0),
+                Block.box(2.0, 0.0, 2.0, 14.0, 11.0, 14.0),
+                Block.box(1.0, 0.0, 1.0, 15.0, 13.0, 15.0),
+                Block.box(0.0, 0.0, 0.0, 16.0, 14.0, 16.0)
+        };
     }
 }
--- src/main/java/net/shadowbeast/projectshadow/blocks/crops/PepperCropBlock.java
+++ src/main/java/net/shadowbeast/projectshadow/blocks/crops/PepperCropBlock.java
@@ -1,20 +1,29 @@
 package net.shadowbeast.projectshadow.blocks.crops;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.shadowbeast.projectshadow.items.ModItems;
 import org.jetbrains.annotations.NotNull;
 
 
 public class PepperCropBlock extends CropBlock {
-    public static final int MAX_AGE = 7; //TODO missing crop stage texture
+    public static final int MAX_AGE = 7;
+    private static final VoxelShape[] SHAPE_BY_AGE;
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 7);
     public PepperCropBlock(Properties pProperties) {
         super(pProperties);
+    }
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE_BY_AGE[this.getAge(pState)];
     }
     @Override
     protected @NotNull ItemLike getBaseSeedId() {
@@ -31,5 +40,18 @@ public class PepperCropBlock extends CropBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(AGE);
+    }
+
+    static{
+        SHAPE_BY_AGE = new VoxelShape[]{
+                Block.box(6.0, 0.0, 6.0, 10.0, 4.0, 10.0),
+                Block.box(5.0, 0.0, 5.0, 11.0, 6.0, 11.0),
+                Block.box(4.0, 0.0, 4.0, 12.0, 9.0, 12.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 12.0, 13.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 15.0, 13.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 15.0, 13.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 15.0, 13.0),
+                Block.box(3.0, 0.0, 3.0, 13.0, 15.0, 13.0)
+        };
     }
 }
