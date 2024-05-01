b11f7d5b03dd10270bb7d31c66cf68b8f5d7e618
Shadowkiller548 <92207062+Shadowkiller548@users.noreply.github.com>
2024-05-01 14:26:51 +0200

    freeze effect

--- src/main/java/net/shadowbeast/projectshadow/effect/FreezeEffect.java
+++ src/main/java/net/shadowbeast/projectshadow/effect/FreezeEffect.java
@@ -1,4 +1,24 @@
 package net.shadowbeast.projectshadow.effect;
-public class FreezeEffect {
 
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+
+public class FreezeEffect extends MobEffect {
+    public FreezeEffect(MobEffectCategory mobEffectCategory, int color) {
+        super(mobEffectCategory, color);
+    }
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        if (!pLivingEntity.level().isClientSide()) {
+            // pAmbient does something, don't know what //TODO we need to add small instant damage like you are freeze burn(three hearts per hit and movement slowdown for 10 second for freeze I and for II 20 seconds, and also adding the snow powder freeze overlay
+            // Also, leave pVisible to "false" so it does not show particles
+            pLivingEntity.addEffect(new MobEffectInstance(MobEffects.HARM, 1, pAmplifier, false, false));
+        }
+        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {return true;}
 }
