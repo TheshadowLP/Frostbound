f7e66d0e787666930d88614fff76d06b6e4a25aa
luhcartimods <luhcartimods@gmail.com>
2024-06-17 20:34:26 +0200

    Added Cooked And Raw Yak Meat

--- src/main/java/net/shadowbeast/arcanemysteries/registries/FoodListRegistry.java
+++ src/main/java/net/shadowbeast/arcanemysteries/registries/FoodListRegistry.java
@@ -3,6 +3,9 @@ package net.shadowbeast.arcanemysteries.registries;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 
 @SuppressWarnings("deprecation")
 public class FoodListRegistry {
@@ -10,4 +13,6 @@ public class FoodListRegistry {
     public static final FoodProperties PEPPER = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.5F).effect(new MobEffectInstance(MobEffects.HUNGER,200,0),0.3F).build();
     public static final FoodProperties STACKED_POTATO = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.5F).build();
     public static final FoodProperties STACKED_BAKED_POTATO = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.8F).build();
+    public static final FoodProperties RAW_YAK_MEAT = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.5f).build();
+    public static final FoodProperties COOKED_YAK_MEAT = (new FoodProperties.Builder()).nutrition(9).saturationMod(1f).build();
 }
