02fc71b0cb192b2f6579eb3e79f5afc37d4ec85e
luhcartimods <luhcartimods@gmail.com>
2024-06-18 17:58:32 +0200

    Re-Added DataGen

---
+++ src/main/java/net/shadowbeast/arcanemysteries/datagen/DataGenerators.java
@@ -0,0 +1,38 @@
+package net.shadowbeast.arcanemysteries.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.shadowbeast.arcanemysteries.ArcaneMysteries;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = ArcaneMysteries.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new RecipeGenerator(packOutput));
+        generator.addProvider(event.includeServer(), LootTableGenerator.create(packOutput));
+
+        generator.addProvider(event.includeClient(), new BlockStateGenerator(packOutput, existingFileHelper));
+        generator.addProvider(event.includeClient(), new ItemModelGenerator(packOutput, existingFileHelper));
+
+        BlockTagGenerator blockTagGenerator = generator.addProvider(event.includeServer(),
+                new BlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
+        generator.addProvider(event.includeServer(), new ItemTagGenerator(packOutput, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));
+
+        generator.addProvider(event.includeServer(), new LootModifierGenerator(packOutput));
+        generator.addProvider(event.includeServer(), new PoiTypeTagsGenerator(packOutput, lookupProvider, existingFileHelper));
+
+        generator.addProvider(event.includeServer(), new WorldGenGenerator(packOutput, lookupProvider));
+    }
+}
---
+++ src/main/java/net/shadowbeast/arcanemysteries/datagen/RecipeGenerator.java
@@ -0,0 +1,42 @@
+package net.shadowbeast.arcanemysteries.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.shadowbeast.arcanemysteries.ArcaneMysteries;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RecipeGenerator extends RecipeProvider implements IConditionBuilder {
+    //private static final List<ItemLike>
+
+    public RecipeGenerator(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+    }
+
+    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        for(ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult,
+                            pExperience, pCookingTime, pCookingSerializer)
+                    .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(pFinishedRecipeConsumer,  ArcaneMysteries.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
+    }
+}
