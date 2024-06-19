357eab1a4c89711c426a725a11aa8ccb06b88ba5
GityBoyy <153344018+GityBoyy@users.noreply.github.com>
2024-06-19 20:08:27 +0530

    Fucked Mixins.

--- src/main/java/net/shadowbeast/arcanemysteries/mixin/LivingEntityMixin.java
+++ src/main/java/net/shadowbeast/arcanemysteries/mixin/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package net.shadowbeast.arcanemysteries.mixin;
 
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
@@ -15,7 +16,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EEntityMixin {
@@ -51,6 +55,33 @@ public abstract class LivingEntityMixin extends EEntityMixin {
         return living.hurt(this.damageSources().freeze(), amount);
     }
 
+    @Inject(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;tryAddFrost()V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void aiStep_inject_2(CallbackInfo ci) {
+        if (!this.level().isClientSide && this.tickCount % 40 == 0 && this.isFullyRoasted() && this.canRoast()) {
+            int j = this.getType().is(EntityTypeTags.FREEZE_HURTS_EXTRA_TYPES) ? 5 : 1;
+
+            float amount = (float)j;
+
+            if ((LivingEntity)(Object)this instanceof Player) {
+                double maxHeat1 = TUtil.firstHeat((Player)(Object)this);
+                double maxHeat2 = TUtil.secondHeat((Player)(Object)this);
+                double maxHeat3 = TUtil.maxHeat((Player)(Object)this);
+                TemperatureData data = EStats.getTemperatureStats((LivingEntity)(Object)this);
+                if (data.getTemperatureLevel() > maxHeat1 && data.getTemperatureLevel() <= maxHeat2) {
+                    amount *= 1;
+                }
+                else if (data.getTemperatureLevel() > maxHeat2 && data.getTemperatureLevel() <= maxHeat3) {
+                    amount *= 10;
+                }
+                else if (data.getTemperatureLevel() > maxHeat3) {
+                    amount *= 100;
+                }
+            }
+
+            this.setSecondsOnFire(5);
+        }
+    }
+
     @Redirect(method = "aiStep", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/LivingEntity;isInPowderSnow:Z"))
     public boolean aiStep_isInPowderSnow_redirect(LivingEntity living) {
         if ((LivingEntity)(Object)this instanceof Player) {
@@ -78,6 +109,21 @@ public abstract class LivingEntityMixin extends EEntityMixin {
             } else {
                 boolean flag = !this.getItemBySlot(EquipmentSlot.HEAD).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.CHEST).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.LEGS).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.FEET).is(ItemTags.FREEZE_IMMUNE_WEARABLES);
                 return flag && super.canFreeze();
+            }
+        }
+    }
+
+    @Override
+    public boolean canRoast() {
+        if (this.isSpectator()) {
+            return false;
+        } else {
+            if ((LivingEntity)(Object)this instanceof Player) {
+                TemperatureData data = EStats.getTemperatureStats((LivingEntity)(Object)this);
+                return data.getTemperatureLevel() > TUtil.firstHeat((Player)(Object)this) && super.canRoast();
+            } else {
+                boolean flag = !this.getItemBySlot(EquipmentSlot.HEAD).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.CHEST).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.LEGS).is(ItemTags.FREEZE_IMMUNE_WEARABLES) && !this.getItemBySlot(EquipmentSlot.FEET).is(ItemTags.FREEZE_IMMUNE_WEARABLES);
+                return flag && super.canRoast();
             }
         }
     }
