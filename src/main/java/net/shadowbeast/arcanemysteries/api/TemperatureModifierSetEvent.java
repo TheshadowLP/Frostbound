e049e5055f57a73a4b1877ebc9fc5ef0f1cf3d92
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-06-21 09:09:04 -0400

    Unhelpful java doc

--- src/main/java/net/shadowbeast/arcanemysteries/api/TemperatureModifierSetEvent.java
+++ src/main/java/net/shadowbeast/arcanemysteries/api/TemperatureModifierSetEvent.java
@@ -6,14 +6,22 @@ import net.shadowbeast.arcanemysteries.temprature.util.TemperatureModifier;
 public class TemperatureModifierSetEvent extends Event {
     TemperatureModifier modifier;
 
+    /**
+     * @param modifier The modifier
+     */
     public TemperatureModifierSetEvent(TemperatureModifier modifier) {
         this.modifier = modifier;
     }
 
+    /**
+     * @return modifier
+     */
     public TemperatureModifier getModifier() {
         return modifier;
     }
-
+    /**
+     * @param modifier Sets the modifier
+     */
     public void setModifier(TemperatureModifier modifier) {
         this.modifier = modifier;
     }
