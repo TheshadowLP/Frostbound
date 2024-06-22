8e85484132dbec15fde5088c8ac34c27da66dcb0
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-06-22 11:21:53 -0400

    config is back

--- src/main/java/net/shadowbeast/arcanemysteries/ArcaneMysteries.java
+++ src/main/java/net/shadowbeast/arcanemysteries/ArcaneMysteries.java
@@ -11,7 +11,9 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -22,6 +24,7 @@ import net.shadowbeast.arcanemysteries.block_entities.screen.CrusherScreen;
 import net.shadowbeast.arcanemysteries.block_entities.screen.WinterFurnaceScreen;
 import net.shadowbeast.arcanemysteries.client.BoatModRenderer;
 import net.shadowbeast.arcanemysteries.client.command.TemperatureCommand;
+import net.shadowbeast.arcanemysteries.config.Config;
 import net.shadowbeast.arcanemysteries.enchant.EnchantmentsRegistry;
 import net.shadowbeast.arcanemysteries.entities.mobs.client.DungeonIceRenderer;
 import net.shadowbeast.arcanemysteries.entities.mobs.client.YakRenderer;
@@ -60,6 +63,7 @@ public class ArcaneMysteries extends MinecraftMod {
     private final ReloadListeners reloadListeners = ReloadListener::id;
     public ArcaneMysteries() {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
         instance = this;
         //BlockRegistry.BLOCKS.register(bus);
         //BlockRegistry.BLOCK_ITEMS.register(bus);
