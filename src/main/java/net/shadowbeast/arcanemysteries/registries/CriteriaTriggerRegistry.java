a9bfef2b77927b95cfa3269973b63b07c6236d8c
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2024-07-08 15:59:40 +0300

    Advancement triggers stuff

--- src/main/java/net/shadowbeast/arcanemysteries/registries/CriteriaTriggerRegistry.java
+++ src/main/java/net/shadowbeast/arcanemysteries/registries/CriteriaTriggerRegistry.java
@@ -1,14 +1,15 @@
 package net.shadowbeast.arcanemysteries.registries;
 
 import net.minecraft.advancements.CriteriaTriggers;
-import net.shadowbeast.arcanemysteries.advancement.MilkCowWithBottle;
-import net.shadowbeast.arcanemysteries.advancement.UseCrusher;
+import net.minecraft.advancements.critereon.PlayerTrigger;
+import net.minecraft.resources.ResourceLocation;
+import net.shadowbeast.arcanemysteries.ArcaneMysteries;
 import net.shadowbeast.arcanemysteries.advancement.UsingStaff;
 
 public class CriteriaTriggerRegistry {
     public static final UsingStaff USING_STAFF = new UsingStaff();
-    public static final UseCrusher USE_CRUSHER = new UseCrusher();
-    public static final MilkCowWithBottle MILK_COW_WITH_BOTTLE = new MilkCowWithBottle();
+    public static final PlayerTrigger USE_CRUSHER = new PlayerTrigger(new ResourceLocation(ArcaneMysteries.MOD_ID,"use_crusher"));
+    public static final PlayerTrigger MILK_COW_WITH_BOTTLE = new PlayerTrigger(new ResourceLocation(ArcaneMysteries.MOD_ID,"milk_cow_with_bottle"));
 
     public static void registerCriteriaTriggers() {
         CriteriaTriggers.register(USING_STAFF);
