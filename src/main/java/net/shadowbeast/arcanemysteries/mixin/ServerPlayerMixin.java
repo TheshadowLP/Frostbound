7740546a05b44da6b51e651406d8f06c74211969
CrownSheep <englanderoren@gmail.com>
2024-07-08 22:41:25 +0300

    Great View From Up Here advancement will no longer trigger when using the Levitation Staff

---
+++ src/main/java/net/shadowbeast/arcanemysteries/mixin/ServerPlayerMixin.java
@@ -0,0 +1,19 @@
+package net.shadowbeast.arcanemysteries.mixin;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.shadowbeast.arcanemysteries.util.interfaces.IFallDamageCancelable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayer.class)
+public class ServerPlayerMixin {
+    ServerPlayer player = ((ServerPlayer)(Object)this);
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/critereon/LevitationTrigger;trigger(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/phys/Vec3;I)V", shift = At.Shift.BEFORE), cancellable = true)
+    private void cancelAdvancement(CallbackInfo ci) {
+        if(((IFallDamageCancelable) player).shouldCancelFallDamage()) {
+            ci.cancel();
+        }
+    }
+}
